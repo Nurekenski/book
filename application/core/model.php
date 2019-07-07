c58675d7538156b9ba49ca1be50d4d70cc75297a
Nurbol <nurbolmaden@gmail.com>
2019-07-08 02:42:07 +0600

    changes in controller

--- application/core/model.php
+++ application/core/model.php
@@ -4,8 +4,9 @@ class Model
         function __construct()
         {
             $this->pdo = new PDO(
-              "mysql:host=localhost;dbname=test;charset=utf8", 
-              "root", "", [
+             
+   "mysql:host=o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=lyiwssei3j3n8cxo;charset=utf8", 
+   "za01nv7v556isrzg", "b0wdepn8u0pd7ykj",[
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false
